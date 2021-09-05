584b5f233f753b35c5e591ed56e96f4f8a63234d
christopherjbanks <christopherjbanks@protonmail.com>
2021-09-05 10:22:23 -0400

    Add text to Icons

--- src/components/skills.js
+++ src/components/skills.js
@@ -23,23 +23,69 @@ const LogoContainer = styled.div`
   padding: 20px;
 `;
 
+const LogoWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+
+  width: 150px;
+`;
+
+const LogoText = styled.p`
+  font-size: 0.75rem;
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
+`;
+
 const Skills = () => {
   return (
     <Wrapper>
       <h1>Skills</h1>
       <LogoContainer>
-        <SiJavascript style={{ height: "40px", width: "40px" }} />
-        <IoLogoNodejs style={{ height: "40px", width: "40px", margin: 0 }} />
-        <IoLogoCss3 style={{ height: "40px", width: "40px", margin: 0 }} />
-        <IoLogoHtml5 style={{ height: "40px", width: "40px", margin: 0 }} />
+        <LogoWrap>
+          <SiJavascript style={{ height: "40px", width: "40px" }} />
+          <LogoText>Javascript</LogoText>
+        </LogoWrap>
+
+        <LogoWrap>
+          <IoLogoNodejs style={{ height: "40px", width: "40px", margin: 0 }} />
+          <LogoText>Node.js</LogoText>
+        </LogoWrap>
+
+        <LogoWrap>
+          <IoLogoCss3 style={{ height: "40px", width: "40px", margin: 0 }} />
+          <LogoText>CSS3</LogoText>
+        </LogoWrap>
+
+        <LogoWrap>
+          <IoLogoHtml5 style={{ height: "40px", width: "40px", margin: 0 }} />
+          <LogoText>HTML5</LogoText>
+        </LogoWrap>
       </LogoContainer>
+
       <LogoContainer>
-        <IoLogoReact style={{ height: "40px", width: "40px", margin: 0 }} />
-        <SiStyledComponents
-          style={{ height: "40px", width: "40px", margin: 0 }}
-        />
-        <SiMongodb style={{ height: "40px", width: "40px", margin: 0 }} />
-        <SiGithub style={{ height: "40px", width: "40px", margin: 0 }} />
+        <LogoWrap>
+          <IoLogoReact style={{ height: "40px", width: "40px", margin: 0 }} />
+          <LogoText>React.js</LogoText>
+        </LogoWrap>
+
+        <LogoWrap>
+          <SiStyledComponents
+            style={{ height: "40px", width: "40px", margin: 0 }}
+          />
+          <LogoText>Styled Components</LogoText>
+        </LogoWrap>
+
+        <LogoWrap>
+          <SiMongodb style={{ height: "40px", width: "40px", margin: 0 }} />
+          <LogoText>MongoDB</LogoText>
+        </LogoWrap>
+
+        <LogoWrap>
+          <SiGithub style={{ height: "40px", width: "40px", margin: 0 }} />
+          <LogoText>Github</LogoText>
+        </LogoWrap>
       </LogoContainer>
     </Wrapper>
   );
